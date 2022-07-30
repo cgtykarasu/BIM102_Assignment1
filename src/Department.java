033b586f59471850436cee5d2bf6186668331106
ÇAĞATAY KARASU <61315022+cgtykarasu@users.noreply.github.com>
2022-07-31 00:03:30 +0300

    ESTU BIM102 Assignment1

---
+++ src/Department.java
@@ -0,0 +1,40 @@
+public class Department {
+
+    protected String name;
+    protected int numberOfWorker;
+    public static int number;
+
+
+    // Do not change anything in this method
+    public String getName() {
+        return name;
+    }
+
+
+    // Fill in the blanks.
+    public Department(String name, int numberOfWorker) {
+        this.name = name;
+        this.numberOfWorker = numberOfWorker;
+        number++;
+    }
+
+    // Erase the content. Fill in the blanks. If name and numberOfWorker are equal, it returns true
+    @Override
+    public boolean equals(Object obj) {
+
+        return (name.equals(((Department) obj).name))&&(numberOfWorker==(((Department) obj).numberOfWorker));
+
+    }
+    // Erase the content. Fill in the blanks. Format: [name]/[numberOfWorker]
+    // For example: Android/number of worker : 30
+    @Override
+    public String toString() {
+
+        return name + "/number of worker : " + numberOfWorker;
+
+    }
+
+
+
+
+}
